4a4b0bb59be994e026d626af73c51d99912653dd
JackShaw2022 <ionathan.m.kelly@gmail.com>
2022-01-17 22:50:40 -0500

    Adding Lists to CounterViewModel

--- app/src/main/java/com/example/viewmodeltwofragments_jan10/ui/viewmodel/CounterViewModel.kt
+++ app/src/main/java/com/example/viewmodeltwofragments_jan10/ui/viewmodel/CounterViewModel.kt
@@ -11,20 +11,29 @@ import com.example.viewmodeltwofragments_jan10.ui.dataclass.UserDataClass
 // VM part of MVVM
 class CounterViewModel: ViewModel() {
 
-    private var _firstName: MutableLiveData<String> = MutableLiveData("")
-    val firstName: LiveData<String> get() = _firstName
+    private var _firstName: MutableLiveData<List<String>> = MutableLiveData()
+    val firstName: LiveData<List<String>> get() = _firstName
+    private val arrFirstName = ArrayList<String>()
 
-    private var _lastName: MutableLiveData<String> = MutableLiveData("")
-    val lastName: LiveData<String> get() = _lastName
+    private var _lastName: MutableLiveData<List<String>> = MutableLiveData()
+    val lastName: LiveData<List<String>> get() = _lastName
+    private val arrLastName = ArrayList<String>()
 
     fun addName() {
         var name = UserDataClass("Jon", "Kelly")
         // Grabbing current value of live data
-        val currentFirstName: String? = firstName.value
-        val currentLastName: String? = lastName.value
+        val currentFirstName: String = name.firstName
+        val currentLastName: String = name.lastName
+
+        arrFirstName.add(currentFirstName)
+        arrLastName.add(currentLastName)
 
         // Updating MutableLiveData with new value
-        _firstName.value = currentFirstName?.plus(name.firstName)
-        _lastName.value = currentLastName?.plus(name.lastName)
+        for(i in 0 until arrFirstName.size) {
+            _firstName.value = arrFirstName
+        }
+        for(j in 0 until arrLastName.size){
+            _lastName.value = arrLastName
+        }
     }
 }
